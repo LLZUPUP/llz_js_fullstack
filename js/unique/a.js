6d922a241c51b7d266af214252c340de7c3efbe0
zll18314831310 <18314831310@163.com>
2018-05-09 22:50:33 +0800

    上传了数组去重

--- js/unique/a.js
+++ js/unique/a.js
@@ -1,1 +1,25 @@
 const arr = [1,2,3,4,1,4,5,3];
+function unique(arr) {
+    // 严谨性
+    if(!Array.isArray(arr)) {
+        console.log('type error');
+        return;
+    }
+    // 准备一个空数组，结果数组，没有重复的元素的
+    const resultArr = [arr[0]];
+    for(let i=1;i<arr.length;i++) {
+        // arr[i]
+        let flag = true;
+        for(let j =0;j<resultArr.length;j++) {
+            if(arr[i] === resultArr[j]) {
+                flag = false;
+                break;
+            }
+        }
+        if(flag) {
+            resultArr.push(arr[i]);
+        }
+    }
+    return resultArr
+}
+console.log(unique(arr))
