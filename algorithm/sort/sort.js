8e5fdf906b37f0bfc08c3fc36e665888b67b6565
zll18314831310 <18314831310@163.com>
2018-05-29 17:10:35 +0800

    桶排序

---
+++ algorithm/sort/sort.js
@@ -0,0 +1,26 @@
+const source_arr = [35,99,18,18,76,12];
+function sort() {
+    const arr = [];
+    // 利用数组的下标，数字大小在下标范围之内
+    // 下标是有顺序的，桶排序
+    const a = [];
+    for(let i = 0;i<100;i++) {
+        a[i]=0;
+    }
+    for(let i = 0;i<source_arr.length;i++) {
+        const item = source_arr[i];
+        a[item]+=1;
+    }
+    for(let i =0;i<a.length;i++) {
+        if(a[i]>=0) {
+            for(let j =0;j<a[i];j++) {
+                arr.push(i);
+            }
+            
+        }
+    }
+
+    return arr;
+}
+const sorted_arr = sort();
+console.log(sorted_arr);
