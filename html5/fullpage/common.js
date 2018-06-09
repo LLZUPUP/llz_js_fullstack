cb4a6124cbc2cd7d05a340b6e47b9e2882162058
zll18314831310 <18314831310@163.com>
2018-06-09 19:46:56 +0800

    全屏滚动轮子

---
+++ html5/fullpage/common.js
@@ -0,0 +1,46 @@
+class PureFullPage {
+    constructor(options) {
+        this.viewHeight = window.innerHeight;
+        this.pureFullPage = document.querySelector('#pureFullPage');
+        this.page = document.querySelectorAll('.page');
+        this.init();
+    }
+    init() {
+        this.pureFullPage.style.height = `${this.viewHeight}px`;
+        // mousewheel时间监听
+        // this.scrollMouse 方法，负责滚动 执行太频繁
+        // throttle 在规定时间里只执行一次
+        // 重新返回一个函数，handleMouseWheel，闭包，将this.scrollMouse 封到内部 
+        // this,函数执行的context
+        // 1000 delay 推迟执行的时间
+        const handleMouseWheel = utils.throttle(this.scrollMouse,this,1500);
+        if(navigator.userAgent.toLowerCase().indexOf('firefox')===-1) {
+            document.addEventListener('mousewheel',handleMouseWheel,false);
+        }else {
+            document.addEventListener('DOMMouseScroll',handleMouseWheel,false);
+        }
+    }
+    // 滚轮事件处理函数
+    scrollMouse(e) {
+        console.log(e)
+        console.log(this.pureFullPage.scrollTop)
+        // console.log(e)
+        let viewHeight = this.viewHeight;
+        // console.log(viewHeight)
+        let top = parseInt(getComputedStyle(this.pureFullPage,null).top);
+        // console.log(top)
+        // for(let i=0;i<this.page.length;i++) {
+            if(e.wheelDeltaY>0) {
+                this.pureFullPage.style.top = (top+viewHeight)+'px'; 
+            }else {
+                this.pureFullPage.style.top -= (top-viewHeight)+'px'; 
+            }
+            // console.log(i)
+        // }
+        
+        
+
+      
+    }
+    
+}
---
+++ html5/fullpage/readme.md
@@ -0,0 +1,6 @@
+手写全屏滚动插件
+
+- 每个页面都是一个屏高
+- 上下切换滚屏，要有transition的效果
+- 分页指示器 
+- 兼容 PC/ Mobile  event 浏览器Chrome safari Mozilla
